57e872496da63ce0b6b7baa8fd685bedd9491851
zxy680pla <zhongxiangyu1@h-partners.com>
2025-11-10 19:21:26 +0800

    update:  rotate_manager.h 全局事件管理
    Signed-off-by: zxy680pla <zhongxiangyu1@h-partners.com>

--- interfaces/innerkits/dock/rotate_manager.h
+++ interfaces/innerkits/dock/rotate_manager.h
@@ -23,21 +23,83 @@
 namespace OHOS {
 class RotateManager : public HeapBase {
 public:
+    /**
+     * @brief Get the singleton instance of RotateManager.
+     *
+     * This function returns the global unique instance of RotateManager.
+     * It is thread-safe and should be used to access the rotation event manager.
+     *
+     * @return RotateManager& Reference to the singleton instance.
+     */
     static RotateManager& GetInstance(void);
- 
-    // 注册、删除、注销监听器
+
+    /**
+     * @brief Register a listener for rotation events.
+     *
+     * Adds a new listener to the internal list. The listener will receive
+     * rotation events once registered.
+     *
+     * @param listener Pointer to the RotateEventListener to register.
+     * @note The listener must remain valid for the duration of registration.
+     */
     void Add(RotateEventListener* listener);
+
+    /**
+     * @brief Remove a listener from rotation event notifications.
+     *
+     * Removes the specified listener from the list of active listeners.
+     * After this call, the listener will no longer receive rotation events.
+     *
+     * @param listener Pointer to the RotateEventListener to remove.
+     * @note The listener must have been previously added.
+     */
     void Remove(RotateEventListener* listener);
+
+    /**
+     * @brief Remove all registered listeners.
+     *
+     * Clears the list of registered listeners, stopping all listeners
+     * from receiving rotation events.
+     */
     void Clear();
- 
-    // 获取已注册的监听器列表
+
+    /**
+     * @brief Get the list of currently registered listeners.
+     *
+     * Returns a const reference to the internal list of registered listeners.
+     * The returned list is read-only and should not be modified directly.
+     *
+     * @return const List<RotateEventListener*>& Reference to the list of listeners.
+     */
     const List<RotateEventListener*>& GetRegisteredListeners() const;
- 
-    // 触发旋转事件（分发给所有注册的监听器）
+
+    /**
+     * @brief Notify all registered listeners that a rotation has started.
+     *
+     * This method triggers the OnRotateStart callback on all registered listeners.
+     *
+     * @param event The rotation event data.
+     */
     void OnRotateStart(const RotateEvent& event);
+
+    /**
+     * @brief Notify all registered listeners of a rotation update.
+     *
+     * This method triggers the OnRotate callback on all registered listeners.
+     *
+     * @param event The rotation event data.
+     */
     void OnRotate(const RotateEvent& event);
+
+    /**
+     * @brief Notify all registered listeners that a rotation has ended.
+     *
+     * This method triggers the OnRotateEnd callback on all registered listeners.
+     *
+     * @param event The rotation event data.
+     */
     void OnRotateEnd(const RotateEvent& event);
- 
+
 private:
     List<RotateEventListener*> rotateList_;
     RotateManager() = default;
